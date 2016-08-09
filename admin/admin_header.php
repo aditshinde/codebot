dbeb6bde00fc341e92c79bd8cf16eb652c7e35a3
Adit Shinde <Adit Shinde>
2016-08-09 23:32:16 +0530

    Added FAB and add_faculty form in admin
    Created a template form for adding new faculties in a Fixed Action Button on bottom right corner as per FAB Material Design guidelines

---
+++ admin/admin_header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="navbar-fixed">
+        <nav class="black darken-2">
+        
+    <div class="nav-wrapper container">
+        
+        <a href="index.php" class="brand-logo">&LT;CodeBot/&GT;</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="../Controller/Logout.php">Logout</a></li>
+      </ul>
+    </div>
+        
+  </nav>
+    </div>        
+</header>
--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,36 @@
         <meta charset="UTF-8">
         <title>Home</title>
         <?php
-        include 'materialize.html';
+        require_once 'materialize.html';
         ?>
     </head>
     <body>
         <?php
-        include '../header.php';
+        require_once 'admin_header.php';
+        require_once 'admin_fab.php';
+        
+        if(isset($_GET['fab']))
+        {
+            if($_GET['fab']==md5(1))
+            {
+                
+            }
+            elseif($_GET['fab']==md5(2))
+            {
+                
+            }
+            elseif($_GET['fab']==md5(3))
+            {
+                require_once 'add_faculty.php';
+            }
+            elseif($_GET['fab']==md5(4))
+            {
+                
+            }
+        }
+        
         ?>
+        
         
     </body>
 </html>
